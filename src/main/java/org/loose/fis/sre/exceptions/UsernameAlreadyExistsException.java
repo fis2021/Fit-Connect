23c284c5d9dd435efb1ace2f0d943f590d86c95e
Bianca Belea <bianca.belea@student.upt.ro>
2021-04-14 23:00:05 +0300

    FC-8 - Implementarea procedurii de Register

---
+++ src/main/java/org/loose/fis/sre/exceptions/UsernameAlreadyExistsException.java
@@ -0,0 +1,16 @@
+package org.loose.fis.sre.exceptions;
+
+public class UsernameAlreadyExistsException extends Exception {
+
+    private String username;
+
+    public UsernameAlreadyExistsException(String username) {
+        super(String.format("Un cont cu numele %s exista deja!", username));
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
